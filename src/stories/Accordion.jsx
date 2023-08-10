f9bbe730aade2935bfe4708c3ececabb0efe0a38
Karsten Bak Malle <Karsten_malle@hotmail.com>
2023-08-10 17:02:03 +0200

    Work on transition for Accordion

--- src/stories/Accordion.jsx
+++ src/stories/Accordion.jsx
@@ -27,21 +27,36 @@ export const Accordion = ({
     }
   };
 
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      accordionToggle();
+    }
+  };
+
   return (
     <div className={`accordion`}>
       <div className={`state-${state}`}>
-        <div className={`accordion-header`} tabindex="0">
+        <div
+          className={`accordion-header`}
+          tabindex="0"
+          onClick={accordionToggle}
+          onKeyDown={handleKeyDown}
+          role="button"
+          aria-expanded={stateToggle}
+        >
           {iconBool && <Icon className="title-icon" />}
-          <div className="title-text">{title}</div>
-          <button onClick={accordionToggle} className="accordion-toggle-button" tabindex="0">
-            {" "}
-            {}
-            {stateToggle === true && <OpenIcon className="open-icon" />}
-            {stateToggle === false && <CloseIcon className="close-icon" />}
-          </button>
+          <div className="title-text">{title}</div> {}
+          <div className="icon-container">
+            <OpenIcon
+              className={`open-icon ${stateToggle ? "visible" : "hidden"}`}
+            />
+            <CloseIcon
+              className={`close-icon ${!stateToggle ? "visible" : "hidden"}`}
+            />
+          </div>
         </div>
       </div>
-      <div className="accordion-content">
+      <div className={`accordion-content-${stateToggle}`}>
         {stateToggle === true && (
           <div className="text">
             {" "}
@@ -65,28 +80,11 @@ export const Accordion = ({
             lacinia dolor. Nam non est urna. Aliquam viverra lectus ut lectus
             ullamcorper, at elementum ante convallis. Fusce aliquet quam sed
             mauris ornare, id facilisis ligula dignissim. Curabitur ac quam eget
-            enim gravida sollicitudin sed accumsan dui. Morbi dapibus orci non
-            mi imperdiet, sed molestie augue fringilla. Suspendisse dictum,
-            sapien sed laoreet cursus, quam turpis cursus metus, eu ullamcorper
-            orci augue vitae dui. Maecenas a diam auctor, sagittis sem ac,
-            gravida mauris. Class aptent taciti sociosqu ad litora torquent per
-            conubia nostra, per inceptos himenaeos. Vivamus vitae nisi in sem
-            interdum maximus. Duis odio neque, vehicula quis semper vitae,
-            hendrerit non erat. Phasellus laoreet aliquet nulla eu luctus. In
-            pharetra vel eros luctus sagittis. Donec rhoncus bibendum quam nec
-            auctor. Etiam ut risus tincidunt, lobortis ante quis, lobortis
-            tortor. Duis nec auctor enim. Interdum et malesuada fames ac ante
-            ipsum primis in faucibus. Sed sed efficitur tellus, vehicula
-            fringilla neque. In sed ex interdum, fringilla ante eget, feugiat
-            turpis. Donec eget finibus dolor, ac fermentum nulla. Fusce a urna
-            ex. Nam pretium aliquet mattis. Fusce tristique nibh lacus, quis
-            varius quam placerat quis. Suspendisse et placerat nunc.{" "}
+            enim gravida sollicitudin sed accumsan dui.{" "}
           </div>
         )}
       </div>
-      <div className="accordion-divider">
-
-        </div>
+      <div className="accordion-divider"></div>
     </div>
   );
 };
